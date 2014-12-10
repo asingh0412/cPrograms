85150473a1b284c19827900a53f14c6528a78073
Amaninder Singh <asingh15@mail.greenriver.edu>
2014-12-10 14:17:11 -0800

    version1
    I changed without pushing it to git so now i pushing back

--- turtle.c
+++ turtle.c
@@ -1,7 +1,7 @@
 //AmaninderSingh
 // Pool puzzle intro to struct 
 // Version 2, 
-// passing values by reference. vs passing by value 
+// passing values by reference. vs passing by value
 
 
 #include <stdio.h>
@@ -14,17 +14,17 @@ typedef struct{
 	int age;
 }turle;
 
-void happy_birthday(turle *t)
+void happy_birthday(turle t)
 {
-	t->age = t->age +1; // or (*t).age
+	t.age = t.age +1;
 	printf("Happy Birthday %s! You are now %i years old!\n",
-	t->name, t->age );
+	t.name, t.age );
 }
 
 int main()
 {
 	turle myrtle = {"Myrtle", "Leatherback sea turtle", 99};
-	happy_birthday(&myrtle);
+	happy_birthday(myrtle);
 	printf("%s's age is now %i\n", myrtle.name, myrtle.age );
 	return 0;
 }
